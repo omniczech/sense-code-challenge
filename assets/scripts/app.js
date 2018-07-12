5d8b0b8c6bfb733d00a1aa69e873eb96f653be19
omniczech <mikepetschek@gmail.com>
2018-07-12 14:13:15 -0400

    Adds highlight JS along with styling
    Found jQuery code to replace <img> tags with a specific class with
    actual <svg> objects allowing for highlight to change the fill of them.
    Once the page loads, it creates an array of the different parameters and
    another function uses the value for tag to add a class to the
    corresponding menu item. The class then adds the Sense red color.

---
+++ assets/scripts/app.js
@@ -0,0 +1,47 @@
+function getUrlVars() {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    console.log(vars);
+    return vars;
+}
+
+function highlightMenu() {
+  // get parameter for tab
+  var itemToHighlight = getUrlVars()["tab"];
+  // find the corresponding DOM element
+  var highlight = document.getElementById(itemToHighlight);
+  // Add a class to highlight it.
+  highlight.classList.add("highlight");
+
+  // Code found on github to convert <img> tags with svgs to actual svg objects.
+  jQuery('img.svg').each(function(){
+      var $img = jQuery(this);
+      var imgID = $img.attr('id');
+      var imgClass = $img.attr('class');
+      var imgURL = $img.attr('src');
+      console.log(imgURL);
+      jQuery.get(imgURL, function(data) {
+          // Get the SVG tag, ignore the rest
+          var $svg = jQuery(data).find('svg');
+
+          // Add replaced image's ID to the new SVG
+          if(typeof imgID !== 'undefined') {
+              $svg = $svg.attr('id', imgID);
+          }
+          // Add replaced image's classes to the new SVG
+          if(typeof imgClass !== 'undefined') {
+              $svg = $svg.attr('class', imgClass+' replaced-svg');
+          }
+
+          // Remove any invalid XML tags as per http://validator.w3.org
+          $svg = $svg.removeAttr('xmlns:a');
+
+          // Replace image with new SVG
+          $img.replaceWith($svg);
+
+      }, 'xml');
+
+  });
+}
